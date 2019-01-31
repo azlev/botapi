4ee47036fa0849d13ddb078ddca7e2cf306a14c6
Jose Arthur Benetasso Villanova <jose.arthur@locaweb.com.br>
2019-01-31 16:00:06 -0200

    Removing the default and the editable=False
    On the previous version the Model ignored the id and
    generated a new one.

--- bots/models.py
+++ bots/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 
 class Bot(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True)
     name = models.CharField(max_length=65535)
