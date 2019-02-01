986924b53b423de9fc01ec5c68dcd2f69fa357b9
Jose Arthur Benetasso Villanova <jose.arthur@gmail.com>
2019-02-01 05:31:40 -0200

    Put from/to logic inside model
    The main idea is to split visions: the bot/direction and client details
    will stay only in the model.
    
    Other parts will see "from_" and to.

--- messages/models.py
+++ messages/models.py
@@ -6,9 +6,11 @@ from bots.models import Bot
 
 
 class Message(models.Model):
+
     class Meta:
         db_table = 'bots_messages'
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid1, editable=False)
     conversationId = models.UUIDField(null=False, db_index=True)
     timestamp = models.DateTimeField(null=False)
     # Instead of "from" and "to", use "bot", "client" and "direction"
@@ -21,3 +23,33 @@ class Message(models.Model):
     direction = models.CharField(null=False, max_length=1, choices=MESSAGE_DIRECTION)
     client = models.UUIDField(null=False)
     text = models.TextField(null=False)
+
+    from_ = None
+    to    = None
+
+    @classmethod
+    def from_db(cls, db, field_names, values):
+        message = super().from_db(db, field_names, values)
+        if message.direction == 'F':
+            message.from_ = message.bot
+            message.to = message.client
+        else:
+            message.to = message.bot
+            message.from_ = message.client
+        return message
+
+    def save(self, *args, **kwargs):
+        try:
+            b = Bot.objects.get(id=self.from_)
+            self.bot = b
+            self.direction = 'F'
+            self.client = self.to
+        except Bot.DoesNotExist:
+            try:
+                b = Bot.objects.get(id=self.to)
+                self.bot = b
+                self.direction = 'T'
+                self.client = self.from_
+            except Bot.DoesNotExists:
+                raise ValueError("Bot not found")
+        super().save(*args, **kwargs)
