46be946491464505453f10b5f8e4d92708233302
Jose Arthur Benetasso Villanova <jose.arthur@gmail.com>
2019-02-01 06:09:51 -0200

    Deal with from_ and from impedance

--- messages/serializers.py
+++ messages/serializers.py
@@ -1,35 +1,27 @@
+from rest_framework import serializers
+
 from bots.models import Bot
 from messages.models import Message
-from rest_framework import serializers
 
 
 class MessageSerializer(serializers.Serializer):
     id = serializers.UUIDField(required=False)
     conversationId = serializers.UUIDField(required=True)
     timestamp = serializers.DateTimeField(required=True)
-    bot = serializers.UUIDField()
-    direction = serializers.CharField(max_length=1)
-    client = serializers.UUIDField()
+    frm = serializers.UUIDField(source='from_')
+    to = serializers.UUIDField()
     text = serializers.CharField(required=True)
 
-    def _direction(self, data, botkey, clientkey, direction):
-        try:
-            b = Bot.objects.get(id=data[botkey])
-            data['bot'] = b.id
-            data['client'] = data[clientkey]
-            data['direction'] = direction
-        except Bot.DoesNotExist:
-            return False
-        return True
+    def create(self, validated_data):
+        print(validated_data)
+        m = Message()
+        m.conversationId = validated_data['conversationId']
+        m.timestamp = validated_data['timestamp']
+        m.from_ = validated_data['from_']
+        m.to = validated_data['to']
+        m.text = validated_data['text']
+        m.save()
 
-    def validate(self, data):
-        """
-        Check and transform from and to in bot, direction and client
-        """
-        print("mimimi", data)
-        if not self._direction(data, 'from', 'to', 'F'):
-            if not self.direction(data, 'to', 'from', 'T'):
-                raise serializers.ValidationError("Bot not found")
-        #del data['from']
-        #del data['to']
-        return data
+        return m
+MessageSerializer._declared_fields['from'] = MessageSerializer._declared_fields['frm']
+del MessageSerializer._declared_fields['frm']
