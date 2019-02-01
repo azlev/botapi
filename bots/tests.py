8108609be9233adad0d52e572bd3b0ac76fae4a7
Jose Arthur Benetasso Villanova <jose.arthur@gmail.com>
2019-02-01 07:19:59 -0200

    /bots tests

--- bots/tests.py
+++ bots/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APIRequestFactory
+
+from .models import Bot
+from .views import BotMain, BotDetail
+
+class BotTests(TestCase):
+
+    def test_botmain_should_accept_only_post(self):
+        factory = APIRequestFactory()
+        request = factory.post('/bots', {'id': '36b9f842-ee97-11e8-9443-0242ac120002', 'name': 'Aureo'})
+        response = BotMain.as_view()(request)
+        self.assertEqual(response.status_code, 201)
+
+        request = factory.get('/bots')
+        response = BotMain.as_view()(request)
+        self.assertEqual(response.status_code, 405)
+
+        request = factory.put('/bots', {'id': '36b9f842-ee97-11e8-9443-0242ac120002', 'name': 'Aureo'})
+        response = BotMain.as_view()(request)
+        self.assertEqual(response.status_code, 405)
+
+        request = factory.delete('/bots')
+        response = BotMain.as_view()(request)
+        self.assertEqual(response.status_code, 405)
+
+    def test_botmain_do_not_duplicate(self):
+        factory = APIRequestFactory()
+        request = factory.post('/bots', {'id': '36b9f842-ee97-11e8-9443-0242ac120002', 'name': 'Aureo'})
+        response = BotMain.as_view()(request)
+        self.assertEqual(response.status_code, 201)
+
+        request = factory.post('/bots', {'id': '36b9f842-ee97-11e8-9443-0242ac120002', 'name': 'Aureo'})
+        response = BotMain.as_view()(request)
+        self.assertEqual(response.status_code, 400)
+
+    def test_botdetail_do_not_update_id(self):
+        factory = APIRequestFactory()
+        request = factory.post('/bots', {'id': '36b9f842-ee97-11e8-9443-0242ac120002', 'name': 'Aureo'})
+        response = BotMain.as_view()(request)
+        request = factory.put('/bots/36b9f842-ee97-11e8-9443-0242ac120002', {'id': '36b9f842-ee97-11e8-9443-0242ac120003', 'name': 'Aureo'})
+
+        response = BotMain.as_view()(request)
+        self.assertEqual(response.status_code, 405)
--- bots/views.py
+++ bots/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-class BotMain(generics.ListCreateAPIView):
+class BotMain(generics.CreateAPIView):
     queryset = Bot.objects.all()
     serializer_class = BotSerializer
 
